cd9110ead4321312267fc9aadebf33719ca9f973
Javier Antonio Rivasseau <113557838+Javier-Rivasseau@users.noreply.github.com>
2023-03-10 15:55:47 -0300

    Update DATA FOR TABLEAU PUBLIC- DATA PARA TABLEAU PUBLIC.sql

--- DATA FOR TABLEAU PUBLIC- DATA PARA TABLEAU PUBLIC.sql
+++ DATA FOR TABLEAU PUBLIC- DATA PARA TABLEAU PUBLIC.sql
@@ -1,3 +1,43 @@
+#Cargando los datos desde mi PC hacia la base de datos en MySQL
+# Loading data in MySQL
+
+load data local infile 'C:\\Users\\Javi\\Desktop\\DATA ANALYSIS PROJECT COVID\\Nashville Project\\Nashville data.csv'
+
+into table nashville_data
+
+fields terminated by ';'
+
+enclosed by '"'
+
+lines terminated by '\n'
+
+ignore 1 rows;
+
+
+-- show global variables like 'local_infile';
+
+-- if it shows-
+
+-- +---------------+-------+
+-- | Variable_name | Value |
+-- +---------------+-------+
+-- | local_infile  |  OFF  |
+-- +---------------+-------+
+-- (this means local_infile is disable)
+
+#mysql> SET GLOBAL local_infile=true;
+-- Query OK, 0 rows affected (0.00 sec)
+
+-- mysql> SHOW GLOBAL VARIABLES LIKE 'local_infile';
+
+-- +---------------+-------+
+-- | Variable_name | Value |
+-- +---------------+-------+
+-- | local_infile  | ON    |
+-- +---------------+-------+
+-- 1 row in set (0.01 sec)
+
+
 # 1- GLOBAL NUMBERS, TOTAL DEATHS, TOTAL CASES, DEATH PERCENTAGE SINCE START TO 7 FEBRUARY 2023
 # NUMEROS GLOBALES, MUERTES TOTALES, CASOS TOTALES, PORCENTAJE DE MORTANDAD DESDE EL INICIO DE LA PANDEMIA HASTA EL 7 DE FEBRERO DE 2023
 
